57188624dc2d85a6084feb440c4dcf8a87a8a59b
Rafael Roberto <rafael.roberto@supportcomm.com.br>
2024-11-15 17:56:39 -0300

    w2 learnig activities CSS

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,7 +1,5 @@
-// Define o ano atual no rodapé
 const currentYear = new Date().getFullYear();
 document.getElementById("currentyear").textContent = currentYear;
 
-// Define a data da última modificação no rodapé
 const lastModified = document.lastModified;
 document.getElementById("lastModified").textContent = `Last Modification: ${lastModified}`;
